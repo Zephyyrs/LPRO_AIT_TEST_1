58f7dd320ed7e9771d68524570546d7532dd930f
Zephyyrs <kevin.luglia@etu.umontpellier.frgit config user.email kevin.luglia@etu.umontpellier.fr>
2020-02-05 11:32:16 +0100

    create function test_button

--- Source/Script/my_script.js
+++ Source/Script/my_script.js
@@ -5,3 +5,15 @@
 function init() {
     document.getElementById("test").innerHTML = "init say Hi !";
 }
+
+/* 
+* test_button
+* called by button onClick
+*/
+
+function test_button(){
+    var my_test_div = document.getElementById("test");
+    my_test_div.innerHTML = "Button clicked!";
+    my_test_div.style.backgroundColor = "green"
+    alert("Clicked");
+}
